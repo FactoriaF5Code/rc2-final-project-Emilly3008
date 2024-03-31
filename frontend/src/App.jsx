325ea6014aaef1760712085701dd5e094322d2d0
Emii <151835514+Emilly3008@users.noreply.github.com>
2024-03-31 17:07:56 +0200

    MAIN, INDEXCSS, APP,CSS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,21 @@
+
 import './App.css'
-import { BrowserRouter, Routes, Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './pages/home/Home'
+import DetailsProduct from "./pages/detailsProduct/DetailsProduct"
+
 function App() {
-  
+
 
   return (
-    
+
     <BrowserRouter >
-    <Routes>
-      <Route  path="/" element={<Home/>}/>
-    </Routes>
-    
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details/:productId" element={<DetailsProduct />} />
+      </Routes>
+
+
     </BrowserRouter>
   )
 }
